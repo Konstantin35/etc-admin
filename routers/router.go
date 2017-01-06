ab19691c7f61e021b530e1af0958017de2f21eb7
taczc64 <gavin@praseodymium.local>
2017-01-06 16:12:05 +0800

    delete router getfee

--- routers/router.go
+++ routers/router.go
@@ -16,8 +16,8 @@ func init() {
 	Routes.HandleFunc("/main/statistic", controller.StatisticData).Methods("GET", "OPTIONS")
 	/****************routers for user manage********************/
 	Routes.HandleFunc("/user/query/{value}", controller.QueryUsers).Methods("GET", "OPTIONS") //query by wallet address or login account or email or phone number
-	Routes.HandleFunc("/user/info/user/settings", controller.SetUserInfo).Methods("PUT")
-	Routes.HandleFunc("/user/info/common/settings/{vip}", controller.SetFee).Methods("PUT") //set common user and vip user pool fee
+	Routes.HandleFunc("/user/info/settings", controller.SetUserInfo).Methods("PUT", "OPTIONS")
+	Routes.HandleFunc("/user/info/common/fees", controller.Fee).Methods("GET", "OPTIONS", "PUT") //set common user and vip user pool fee
 	Routes.HandleFunc("/user/data/chart/{address:0x[0-9a-fA-F]{40}}", controller.GetAddressChartData).Methods("GET", "OPTIONS")
 	Routes.HandleFunc("/user/data/statistic/{address:0x[0-9a-fA-F]{40}}", controller.GetAddressStaticData).Methods("GET", "OPTIONS")
 	Routes.HandleFunc("/user/info/miners/{address:0x[0-9a-fA-F]{40}}", controller.GetMinersInfo).Methods("GET", "OPTIONS")
